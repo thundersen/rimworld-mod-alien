cd2fd6795835520d1b227ac5cae30683a1f87ab6
thundersen <thundersen@posteo.de>
2018-03-25 17:18:00 +0200

    moved hiding place search from JobGiver to ChestbursterPawn class and cleaned up some more

--- src/Source/ChestbursterJobGiver.cs
+++ src/Source/ChestbursterJobGiver.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
 using Verse.AI;
 
@@ -16,62 +14,20 @@ namespace Alien
         
         protected override Job TryGiveJob(Pawn pawn)
         {
-            if (pawn.CurJob?.def == JobDefOf.THU_GoIntoHiding)
+            var chestburster = pawn as ChestbursterPawn;
+            
+            if (chestburster == null || chestburster.CurJob?.def == JobDefOf.THU_GoIntoHiding)
                 return null;
             
-            Log.Message("HIDING!");
+            Log.Message("Chestburster trying to hide...");
 
-            var hidingPlace = FindHidingPlace(pawn);
+            var hidingPlace = chestburster.FindHidingPlace();
 
             if (hidingPlace == null) return null;
             
             Log.Message("...in " + hidingPlace.Label);
             
             return new Job(JobDefOf.THU_GoIntoHiding, hidingPlace.Position);
-        }
-        
-        private static Thing FindHidingPlace(Pawn pawn)
-        {
-            const int maxDistance = 18;
-
-            var awesomeHidingPlace = ClosestThingReachable(pawn, maxDistance, IsAwesomeHidingPlace);
-            
-            return awesomeHidingPlace ?? ClosestThingReachable(pawn, maxDistance, IsOKHidingPlace);
-        }
-
-        private static Thing ClosestThingReachable(Pawn pawn, int maxDistance, Predicate<Thing> filter)
-        {
-            return GenClosest.ClosestThingReachable(pawn.Position, pawn.Map,
-                ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.OnCell, TraverseParms.For(pawn),
-                maxDistance, filter);
-        }
-
-        private static bool IsAwesomeHidingPlace(Thing thing)
-        {
-            return typeof(Building_Vent) == thing.def.thingClass || 
-                IsAnyOf(thing, 
-                    ThingDefOf.Ship_Beam, 
-                    ThingDefOf.Ship_Reactor,
-                    ThingDefOf.Ship_CryptosleepCasket,
-                    ThingDefOf.Ship_ComputerCore,
-                    ThingDefOf.Ship_Engine,
-                    ThingDefOf.Ship_SensorCluster);
-        }
-
-        private static bool IsOKHidingPlace(Thing thing)
-        {
-            return IsAnyOf(thing, 
-                ThingDefOf.Grave, 
-                ThingDefOf.SolarGenerator,
-                ThingDefOf.GeothermalGenerator, 
-                ThingDefOf.Cooler, 
-                ThingDefOf.VanometricPowerCell,
-                ThingDefOf.CommsConsole);
-        }
-        
-        private static bool IsAnyOf(Thing thing, params ThingDef[]  thingDefs)
-        {
-            return thingDefs.Select(td => td.GetType()).Contains(thing.def.GetType());
         }
     }
 }
--- src/Source/ChestbursterPawn.cs
+++ src/Source/ChestbursterPawn.cs
@@ -1,4 +1,6 @@
-﻿using RimWorld;
+﻿using System;
+using System.Linq;
+using RimWorld;
 using UnityEngine;
 using Verse;
 using Verse.AI;
@@ -7,18 +9,24 @@ namespace Alien
 {
     public class ChestbursterPawn : Pawn
     {
+        //can be set for debugging and must be null for release
+        private readonly int? overrideXenoSpawnDelay = 2500;
+        
+        private const int maxDistanceVisible = 5;
+        private const int maxDistanceForHidingPlace = 18;
+        private const int maxDistanceForXenoSpawnWhenHiding = 30;
+        
         private Map mapLastSeen;
         private bool hasDisappeared;
         
+
         public int  SpawnXenoAtTick { get; }
         public Map  SpawnXenoOnMap => Map ?? mapLastSeen;
 
-
         public ChestbursterPawn()
         {
-            SpawnXenoAtTick =
-                Find.TickManager.TicksGame +
-                2500; //GameTime.RandomTickNextNight(Find.TickManager.TicksGame, GenLocalDate.HourOfDay(this)),
+            SpawnXenoAtTick = Find.TickManager.TicksGame +
+                overrideXenoSpawnDelay ?? GameTime.RandomTickNextNight(Find.TickManager.TicksGame, GenLocalDate.HourOfDay(this));
 
             Log.Message("Will spawn xeno at " + SpawnXenoAtTick + " (now its " + Find.TickManager.TicksGame + ")");
             
@@ -41,10 +49,15 @@ namespace Alien
         public IntVec3 FindXenoSpawnPosition()
         {
             return hasDisappeared
-                ? CellFinderLoose.RandomCellWith(p => p.DistanceTo(Position) < 30, mapLastSeen) 
+                ? CellFinderLoose.RandomCellWith(IsSuitableXenoSpawnPosition, mapLastSeen) 
                 : Position;
         }
-        
+
+        private bool IsSuitableXenoSpawnPosition(IntVec3 p)
+        {
+            return p.DistanceTo(Position) < maxDistanceForXenoSpawnWhenHiding;
+        }
+
         public void DisappearInHidingPlace()
         {
             mapLastSeen = Map;
@@ -53,21 +66,25 @@ namespace Alien
             
             hasDisappeared = true;
 
-            Messages.Message("Chestburster is hiding now and can't be found before transforming".Translate(),
-                MessageTypeDefOf.NegativeEvent);
+            Messages.Message("THU_Chestburster_InHidingPlace".Translate(), MessageTypeDefOf.NegativeEvent);
         }
 
         private bool IsOutOfSight()
         {
-            const int maxDistance = 5;
-            var anythingThatPreventsHiding = GenClosest.ClosestThingReachable(Position, Map, ThingRequest.ForGroup(ThingRequestGroup.Pawn), PathEndMode.OnCell, TraverseParms.For(this, Danger.Deadly, TraverseMode.PassDoors), maxDistance, t => PreventsHiding(t, this));
+            var anythingThatPreventsHiding = 
+                GenClosest.ClosestThingReachable(Position, Map, 
+                    ThingRequest.ForGroup(ThingRequestGroup.Pawn), 
+                    PathEndMode.OnCell, 
+                    TraverseParms.For(this, Danger.Deadly, TraverseMode.PassDoors), 
+                    maxDistanceVisible, 
+                    PreventsHiding);
 
             return anythingThatPreventsHiding == null;
         }
 
-        private static bool PreventsHiding(Thing other, Thing chestburster)
+        private bool PreventsHiding(Thing other)
         {
-            if (other == chestburster || other.Faction?.def?.defName == chestburster.Faction?.def?.defName)
+            if (other == this || other.Faction?.def?.defName == Faction?.def?.defName)
             {
                 return false;
             }
@@ -76,6 +93,46 @@ namespace Alien
             return otherPawn != null &&
                    otherPawn.Spawned &&
                    !otherPawn.RaceProps.Animal;
+        }
+        
+        public Thing FindHidingPlace()
+        {
+            return FindClosest(IsAwesomeHidingPlace) ?? FindClosest(IsOKHidingPlace);
+        }
+
+        private Thing FindClosest(Predicate<Thing> filter)
+        {
+            return GenClosest.ClosestThingReachable(Position, Map,
+                ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.OnCell, TraverseParms.For(this),
+                maxDistanceForHidingPlace, filter);
+        }
+
+        private static bool IsAwesomeHidingPlace(Thing thing)
+        {
+            return typeof(Building_Vent) == thing.def.thingClass || 
+                   IsAnyOf(thing, 
+                       ThingDefOf.Ship_Beam, 
+                       ThingDefOf.Ship_Reactor,
+                       ThingDefOf.Ship_CryptosleepCasket,
+                       ThingDefOf.Ship_ComputerCore,
+                       ThingDefOf.Ship_Engine,
+                       ThingDefOf.Ship_SensorCluster);
+        }
+
+        private static bool IsOKHidingPlace(Thing thing)
+        {
+            return IsAnyOf(thing, 
+                ThingDefOf.Grave, 
+                ThingDefOf.SolarGenerator,
+                ThingDefOf.GeothermalGenerator, 
+                ThingDefOf.Cooler, 
+                ThingDefOf.VanometricPowerCell,
+                ThingDefOf.CommsConsole);
+        }
+        
+        private static bool IsAnyOf(Thing thing, params ThingDef[]  thingDefs)
+        {
+            return thingDefs.Select(td => td.GetType()).Contains(thing.def.GetType());
         }
     }
 }
